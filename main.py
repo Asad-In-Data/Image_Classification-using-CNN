b1310e76778391e6467eda58d55b5baf4c9e905b
Asad Ali <141873003+Asad-In-Data@users.noreply.github.com>
2025-09-03 01:03:27 +0500

    Add CNN model training and save to main.py
    Added code to main.py to define, compile, train, evaluate, and save a convolutional neural network model for image classification. Limited the dataset size for both training and testing. The trained model is saved as My_model.keras.

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ class_names = ['airplane', 'automobile', 'bird',
                'cat', 'deer', 'dog', 'frog',
                  'horse', 'ship', 'truck']
 
+train_images=train_images[:20000]
+train_labels=train_labels[:20000]
+test_images=test_images[:40000]   
+test_labels=test_labels[:40000]
+
 for i in range(16):
     plt.subplot(4, 4, i + 1)
     plt.imshow(train_images[i])
@@ -20,3 +25,23 @@ for i in range(16):
     plt.xlabel("Class: " + class_names[train_labels[i][0]])
     plt.axis("off")
 plt.show()
+
+
+
+model=models.Sequential()
+model.add(layers.Conv2D(32,(3,3),activation='relu',input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64,activation='relu'))
+model.add(layers.Dense(10,activation='softmax'))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(train_images,train_labels,epochs=10,batch_size=64)
+model.evaluate(test_images,test_labels)
+
+model.save('My_model.keras') 
